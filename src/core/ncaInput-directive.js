97f072ccda0f4e734e42b609edb724a41cea44b6
Philipp Denzler <philippd@gmail.com>
2014-03-21 02:21:40 +0100

    refactor(core): extract message display logic from ncaInput

--- src/core/ncaInput-directive.js
+++ src/core/ncaInput-directive.js
@@ -1,14 +1,13 @@
 angular.module('ncaModelValidation')
 
-  .directive('input', function (ncaValidator, $compile) {
+  .directive('input', function (ncaValidator) {
     return  {
       restrict: 'E',
-      require: ['?^ncaFormType', '?^ngModel', '?^form'],
+      require: ['?^ncaFormType', '?^ngModel'],
       link: function (scope, element, attrs, controllers) {
 
         var ncaFormTypeController = controllers[0],
           ngModelController = controllers[1],
-          formController = controllers[2],
           fieldName = attrs.name;
 
         // Stop right here if this is an <input> that's either not inside of a nca-form-type block
@@ -34,17 +33,6 @@ angular.module('ncaModelValidation')
 
         ngModelController.$parsers.push(validator);
         ngModelController.$formatters.push(validator);
-
-        // Add validation message, this could be easily extracted to an optional module in a second 'input' directive.
-        if (angular.isUndefined(attrs.noMessage)) {
-          var fieldRef = formController.$name + '.' + fieldName;
-          var validationMessageMarkup =
-            '<div ng-show="' + fieldRef + '.$invalid" nca-form-message="' + fieldRef + '"></div>';
-          var validationMessageElement = angular.element(validationMessageMarkup);
-          element.after(validationMessageElement);
-          $compile(validationMessageElement)(scope);
-        }
-
       }
     };
   });
