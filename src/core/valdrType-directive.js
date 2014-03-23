09f184b11c4ff90e7deed25d74d2a35702a75846
Philipp Denzler <philippd@gmail.com>
2014-03-23 12:50:22 +0100

    chore(docs): add basic docs to valdrType directive

--- src/core/valdrType-directive.js
+++ src/core/valdrType-directive.js
@@ -1,5 +1,9 @@
 angular.module('valdr')
 
+  /**
+   * The valdrType directive defines the type of the model to be validated.
+   * The directive exposes the type through the controller to allow access to it by wrapped directives.
+   */
   .directive('valdrType', function () {
     return  {
       controller: function() {
